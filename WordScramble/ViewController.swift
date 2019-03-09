bc52a621ac7f99bdbca7990875dbeffa05dce175
JohnMutina <johnmutina@gmail.com>
2019-03-09 14:30:26 +0100

    Create variable, store words and set initial game state

--- WordScramble/ViewController.swift
+++ WordScramble/ViewController.swift
@@ -8,11 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    // initialize two arrays of strings
+    var allWords = [String]()
+    var usedWords = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // find a file start.txt if existing
+        if let startWordPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+            // if it has strings inside
+            if let startWords = try? String(contentsOfFile: startWordPath) {
+                // break them up when you find a return and store them in the array
+                allWords = startWords.components(separatedBy: "\n")
+            // otherwise
+            } else {
+                // use a default parameter
+                allWords = ["silkworm"]
+            }
+        }
+        // load the initial game state
+        startGame()
+    }
+    
+    // MARK: Define the initial game state
+    func startGame() {
+        // set view title equals to a random word
+        title = allWords.randomElement()
+        // empty the usedWords array
+        usedWords.removeAll(keepingCapacity: true)
+        // reload the tableview data
+        tableView.reloadData()
     }
 
 
